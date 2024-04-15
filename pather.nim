f1b2cc0c0b2123e5af3ddd2c5ce5a99ae170516e
Zander Hill <zander@xargs.io>
2024-04-15 01:50:24 -0500

    Update pather

--- pather.nim
+++ pather.nim
@@ -1,11 +1,18 @@
 #!/usr/bin/env nim
 
 import std/os
+import std/algorithm
 import std/strutils
 import std/sugar
 import std/sequtils
 import std/strformat
 import std/logging
+
+type PriorityPath = tuple
+  path: string
+  priority: int
+
+var paths: seq[PriorityPath]
 
 # Rewrite using std/with https://nim-lang.github.io/Nim/with.html
 iterator reverse*[T](a: seq[T]): T {.inline.} =
@@ -13,6 +20,9 @@ iterator reverse*[T](a: seq[T]): T {.inline.} =
     while i > -1:
         yield a[i]
         dec(i)
+
+proc byWeight(a, b: PriorityPath): int =
+  cmp(b.priority, a.priority)
 
 proc pather(separator = ":", debug = false, frontloadDirs: seq[string]): int =
   var lvl: Level
@@ -26,17 +36,26 @@ proc pather(separator = ":", debug = false, frontloadDirs: seq[string]): int =
 
   var returnCode = 0
   var path = getEnv("PATH")
-  var paths = path.split(separator)
-  var px = collect:
-    for p in paths:
-      absolutePath p
+  var pathsRaw = path.split(separator)
+  for p in deduplicate(pathsRaw):
+    if p.startsWith(getEnv("HOME")):
+      paths.add((absolutePath p, 200))
+    elif p.startsWith("/usr/local"):
+      paths.add((absolutePath p, 100))
+    elif p.startsWith("/opt/homebrew"):
+      paths.add((absolutePath p, 50))
+    else:
+      paths.add((absolutePath p, 1))
 
-  for p in reverse(frontloadDirs):
-    px.insert(p, 0)
-  debug(&"Paths: {px}")
+  algorithm.sort(paths, byWeight)
+  var newPaths: seq[string] = @[]
+  for p in paths:
+    newPaths.add(p.path)
 
-  let dedupedPx = deduplicate(px)
-  echo(join(dedupedPx, ":"))
+  # TODO: determine how to insert with priority
+  for d in frontloadDirs:
+    newPaths.insert(d, 0)
+  echo(join(newPaths, ":"))
   return min(127, returnCode)
 
 import cligen; dispatch pather
